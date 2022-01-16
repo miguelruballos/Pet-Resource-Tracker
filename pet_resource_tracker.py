a366828fbccf3c81f7d9e50f71e9ed9954dd5840
miguelruballos <97655372+miguelruballos@users.noreply.github.com>
2022-01-16 01:04:18 -0800

    Update pet_resource_tracker.py
    Added clarifying comments. Deleted unnecessary pieces of code. Updated LCD print outs that are consistent in formatting. Added loading LCD prints. Added counter that tracks number of loops so that I could print only on the first loop.  Added functions for printouts for cleaner code.

--- pet_resource_tracker.py
+++ pet_resource_tracker.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
 #scale and button imports
-from gpiozero import Button
 import RPi.GPIO as GPIO                # import GPIO
 from hx711 import HX711                # import the class HX711
 from time import sleep
 from rpi_lcd import LCD
 
-#api imports
-
+#Google API imports
 import os.path
 import time
 from google.auth.transport.requests import Request
@@ -20,84 +18,124 @@ from datetime import date
 from datetime import datetime
 
 
-GPIO.setmode(GPIO.BCM)                 # set GPIO pin mode to BCM numbering
+GPIO.setmode(GPIO.BCM)# set GPIO pin mode to BCM numbering
 GPIO.setwarnings(False)
 
 lcd = LCD() #initializing screen
 
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)#red button setup, currently the state is up.
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)#blue button
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)#yellow button
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)#green button
-GPIO.setup(19, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
+lcd.text('Setting up ', 1)
+lcd.text('Device .    ', 2)
+
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)# init red button
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)# init blue button
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)# init yellow button
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)# init green button
+GPIO.setup(26, GPIO.OUT)# init yellow LED
 
 
 #setting up event detection to eliminate holding button or having the program miss the button press.
 GPIO.add_event_detect(21, GPIO.RISING)
 GPIO.add_event_detect(16, GPIO.RISING)
-#GPIO.add_event_detect(13, GPIO.RISING)
 GPIO.add_event_detect(20, GPIO.RISING)
 
 
-
-
-
+#weight scale init
 hx = HX711(dout_pin=5, pd_sck_pin=6)
-GPIO.setmode(GPIO.BCM);
 onehx= HX711(dout_pin=5, pd_sck_pin=6);
 twohx = HX711(dout_pin=23, pd_sck_pin=24);
 
+lcd.text('Setting up ', 1)
+lcd.text('Device . .   ', 2)
 
-
-def zero_scales():    
+def zero_scales():
+    lcd.clear()
+    lcd.text('Set Zero:  ', 1)
+    lcd.text('Set Ratio: ', 2)
     err = onehx.zero();
     err = twohx.zero();
+    lcd.text('Set Zero: Sucess ', 1)
+    lcd.text('Set Ratio: ', 2)
+    sleep(2)
     set_calculate_ratio()#calling the ratio so it can be calculated again.
 
 def set_calculate_ratio():
-    scale_one_ratio= 10549/22.68 #used ike dollar for ratio
+    #Calculate ratio with known weight(Ike dollar coin)
+    scale_one_ratio= 10549/22.68 
     scale_two_ratio= 10405/22.68
-
+    
+    #Set Ratio
     onehx.set_scale_ratio(scale_one_ratio)
     twohx.set_scale_ratio(scale_two_ratio)
     
+#    lcd.clear()
+    lcd.text('Set Zero: Sucess', 1)
+    lcd.text('Set Ratio:Sucess', 2)
+    
 def set_max():
+    #init max variables from current weight reading
     max_food= converted_reading_1
     max_water= converted_reading_2
-    #print('max food: ', max_food, 'max water: ' ,max_water)
-    lcd.clear()
-    string_food_max = str(max_food)#Converting to string to be able to print it on LCD
-    string_water_max = str(max_water)#Converting to string to be able to print it on LCD
-    message_food_max = "Max Food: " + string_food_max + " g"
+    
+    #convert float to string
+    string_food_max = str(max_food)
+    string_water_max = str(max_water)
+    
+    #preparing LCD messsage with string variable
+    message_food_max = "Max Food:  " + string_food_max + " g"
     message_water_max = "Max Water: " + string_water_max + " g"
+    
+    lcd.clear()
     lcd.text(message_food_max, 1)
     lcd.text(message_water_max, 2)
     sleep(2)
+    #return(max_food, max_water)
+    
+    
+#These LCD messages run outside of threaded loop to eliminate LCD conflicts with main Try loop.   
+def message_disable_upload():
     lcd.clear()
-    return(max_food, max_water)
-
-running = False #setting state for upload_data()
-
+    lcd.text('Tracking', 1)
+    lcd.text('Disabled', 2)
+    
+    #turn off yellow light
+    sleep(3)
+def message_set_max_error():
+    lcd.clear()
+    lcd.text('Set Maximum', 1)
+    lcd.text('before tracking', 2)
+    sleep(3)
+    
+def message_enable_upload():
+    lcd.clear()
+    lcd.text('Tracking', 1)
+    lcd.text('Enabled', 2)
+    GPIO.output(26, GPIO.HIGH)
+    sleep(3)
 
 def upload_data(channel):
-    lcd.clear()
-    #print(running, "test2")
-    lcd.text('Initilizing Tracking...', 1)
-    sleep(2)
-    lcd.clear()
     running = True
+    #Init counter for tracking number of loops ran
+    count = 0
     try:
         while running == True:
-            GPIO.output(26, GPIO.HIGH)#turn on light
+            #Check if max variable exist before running upload code. Trigger exception.
+            exception_error_check = max_water
+            
+            #Increment tracker by 1
+            count += 1
+            #Execute LCD message only once at the start
+            if(count == 1):
+                message_enable_upload()
+            
+            
             SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+            #init json file located in same folder as program. Use for credentials.
             SERVICE_ACCOUNT_FILE = 'keys.json'
-
             creds = None
             creds = service_account.Credentials.from_service_account_file(
                     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-            # The ID and range of a sample spreadsheet.
+            # The ID of sample spreadsheet.
             SAMPLE_SPREADSHEET_ID = '1utan0_ss9NDG4OpndFdfr_vF4kjsgwJm2PpTewCRu7Q'
 
 
@@ -106,77 +144,76 @@ def upload_data(channel):
             # Call the Sheets API
             sheet = service.spreadsheets()
             
-            #setting up rows
+            #set row dates
             today = date.today()
             d1 = today.strftime("%m/%d/%y")
             now = datetime.now()
             time = now.strftime("%H:%M:%S")
             
+            #set up row format
             row = [[d1, time, converted_reading_1, converted_reading_2, max_food,max_water]]
             
-            #uploading rows
+            #uploading row
             request = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                       range="Sheet1!A2", valueInputOption="USER_ENTERED",
-                                      body={"values":row}).execute()
-            #Exit the loop by making running False with green button.
-            if not GPIO.input(21):
-                running = False
-                lcd.clear()
-                lcd.text('Tracking ended', 1)
-                sleep(2)
-                lcd.clear()
-                GPIO.output(26, GPIO.LOW)#turn on light
-
+                                      body={"values":row}).execute()              
             sleep(10)
+            #Exit loop with Red Button Press
+            if not GPIO.input(21):
+                #Set running variable to false to exit upload loop
+                running = False 
+                GPIO.output(26, GPIO.LOW)
+                message_disable_upload()
+    
     except NameError:
-                lcd.clear()
-                lcd.text('Set maximum', 1)
-                lcd.text('before tracking', 2)
-                sleep(2)
-                lcd.clear()
-                GPIO.output(26, GPIO.LOW)#turn on light
+        message_set_max_error()
+        #turn off light
+        GPIO.output(26, GPIO.LOW)
+        running = False
 
-
+#Init threaded event for upload
 GPIO.add_event_detect(13, GPIO.RISING, callback=upload_data,bouncetime=200)
-        
 
+        
+lcd.text('Setting up ', 1)
+lcd.text('Device . . .', 2)
 
 #executing initial functions
 zero_scales()
 set_calculate_ratio()
 
+#init upload variable to flase
+running = False
 
+
+
+#Continuous loop to pull most recent weight reading and catch button press events. 
 try:
     i = True
     while i:
         converted_reading_1 = round(onehx.get_weight_mean(30))
         converted_reading_2 = round(twohx.get_weight_mean(30))
     
-        string_reading_1 = str(converted_reading_1)#Converting to string to be able to print it on LCD
-        string_reading_2 = str(converted_reading_2)#Converting to string to be able to print it on LCD
+        #convert float to string for LCD
+        string_reading_1 = str(converted_reading_1)
+        string_reading_2 = str(converted_reading_2)
         
-        lcd.clear()
+        #preparing LCD messsage with string variable
         message_scale_1 = "Scale 1: " + string_reading_1 + " g"
         message_scale_2 = "Scale 2: " + string_reading_2 + " g"
+        
+        lcd.clear()
         lcd.text(message_scale_1, 1)
         lcd.text(message_scale_2, 2)
         
-        #if not GPIO.input(21):#this is another option to caputre the button input but it can be missed by the program.
-        #GPIO.output(19, GPIO.HIGH)#turn on light
-        if GPIO.event_detected(20):#this is a better solution because the program can detect a push even if it was working on something else.
+        if GPIO.event_detected(20):
             zero_scales()
-            lcd.clear()
-            lcd.text('Zeroing', 1)
-            lcd.text('Successfull', 2)
-            sleep(2)
-            #GPIO.output(19, GPIO.LOW)#turn off light
-            lcd.clear()
+            
         if GPIO.event_detected(16):
             set_max()
             max_food= converted_reading_1
             max_water= converted_reading_2
-            sleep(1)
+            
 except KeyboardInterrupt:
-    print('interrupted!')
     lcd.clear()
     GPIO.output(26, GPIO.LOW)#turn on light
